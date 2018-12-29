53c309c551fedbf4aecb06d68750763b2640287f
Your Name <ritchycal@gmail.com>
2018-12-28 19:07:09 -0500

    added frame work for account and main

---
+++ Account.java
@@ -0,0 +1,13 @@
+//super class of so we can manipulae Accounts
+public class Account{
+  String fname="";
+  String lname=""
+  String empilid="";
+  public Account(String empilid){
+    this.empilid=empilid;
+  }
+  public Account(String fname String lname){
+    this.fname=fname;
+    this.lname=lname;
+  }
+}
